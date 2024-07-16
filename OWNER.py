baae1677e4f06225752e6a25005eed1dca6d4a0f
Crnoour <sadvot111@gmail.com>
2024-07-16 23:27:16 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["M_9_T", "M_9_T"]
 OWNER_NAME = "𓏺 َِ𝘾َِ𝘳- َِ𝙉َِ𝘰َِ𝙐َِ𝘳 Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
-BOT_TOKEN = "6525182940:AAENNwzKCmKEJvPppg_6DKanx8Z80XepZ2M"
+BOT_TOKEN = "6243774208:AAHQ2axvnzieUDWbCfymZ-zDstRzpKjDOuw"
 DATABASE = "mongodb+srv://shadow:BE_XB@cluster0.noovm8n.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/vzo_a"
 GROUP = "https://t.me/cr_nox"
