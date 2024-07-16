804afc97f1505db3bb81ce98073184be92c489de
Crnoour <sadvot111@gmail.com>
2024-07-16 23:05:16 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -2,7 +2,7 @@
 OWNER = ["M_9_T", "M_9_T"]
 OWNER_NAME = "𓏺 َِ𝘾َِ𝘳- َِ𝙉َِ𝘰َِ𝙐َِ𝘳 Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
 BOT_TOKEN = "6525182940:AAENNwzKCmKEJvPppg_6DKanx8Z80XepZ2M"
-DATABASE = ""
+DATABASE = "mongodb+srv://shadow:BE_XB@cluster0.noovm8n.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/vzo_a"
 GROUP = "https://t.me/cr_nox"
 VIDEO = "https://t.me/vzo_a/19"
