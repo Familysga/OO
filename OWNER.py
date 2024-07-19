b8a81611ca42eb8e6429323d2748df71588a3a3a
Crnoour <sadvot111@gmail.com>
2024-07-19 18:11:01 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,5 +1,5 @@
 # متطلبات التنصيب #
-OWNER = ["M_9_T", "N_7_k"]
+OWNER = ["M_9_T", "M_9_T"]
 OWNER_NAME = "𓏺 َِ𝘾َِ𝘳- َِ𝙉َِ𝘰َِ𝙐َِ𝘳 Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
 BOT_TOKEN = "7164031548:AAHl4P3u4BijUc9lATwuiLzCPpgLA9bNIqs"
 DATABASE = "mongodb+srv://vv1vbvg:tahLdAddP2xj6R5b@cluster0.4xpikbs.mongodb.net"
