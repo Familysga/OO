5442f5839135dc57fee73c38b2b7836b772ff08f
Crnoour <sadvot111@gmail.com>
2024-07-19 18:10:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = Client(
     "mo",
     api_id="15837209",
     api_hash="fe081df6989a4d79c1004903bb4f23e6",
-    bot_token="6525182940:AAHVo-EPIHrKMYXa5CyfI53_BcHVkhw80bI",
+    bot_token="7164031548:AAHl4P3u4BijUc9lATwuiLzCPpgLA9bNIqs",
     plugins=dict(root="Maker")
     )
 
