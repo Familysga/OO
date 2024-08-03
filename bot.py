d891eb429033aa4a2e18f62ef07b663bad5f3ed9
Familysga <134228968+Familysga@users.noreply.github.com>
2024-08-03 21:45:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,16 +6,16 @@ from pyromod import listen
 
 bot = Client(
     "mo",
-    api_id="15837209",
-    api_hash="fe081df6989a4d79c1004903bb4f23e6",
-    bot_token="7164031548:AAHl4P3u4BijUc9lATwuiLzCPpgLA9bNIqs",
+    api_id="29400566",
+    api_hash="8fd30dc496aea7c14cf675f59b74ec6f",
+    bot_token="6711048210:AAGeTTCLCXyBmcgcgC264YPkmI5g5xBJSnQ",
     plugins=dict(root="Maker")
     )
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
-    AFROTOO = "M_9_T"
+    AFROTOO = "H_8_o"
     await bot.send_message(AFROTOO, "**تم تشغيل ال صانع بنجاح عزيزي المطور ...🥀،**")
     print("[INFO]: تم تشغيل الصانع وارسال رسالة للمطور⚡🚦.")
     await idle()
